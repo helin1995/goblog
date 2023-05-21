a886e4d943b982e71ad4b704e5550589e366fca4
helin1995 <helin.nlp@gmail.com>
2023-05-21 21:23:19 +0800

    新增多页面访问

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func handelerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客使用一记录编程笔记，如您有反馈或建议，请联系"+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到：(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
+
 }
 
 func main() {
